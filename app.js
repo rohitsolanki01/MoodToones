db7fac6ec7ead53abca4ecceb991d35e4f8696ed
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-04-07 22:54:42 +0530

    plalist folder add with thire files

---
+++ app.js
@@ -0,0 +1,29 @@
+
+// const focusCard = document.getElementById('six-card');
+
+// // Add click event listener
+// focusCard.addEventListener('click', function() {
+//     window.open('focus-playlist.html', '_blank');
+// });
+
+// const calmCard = document.getElementById("first");
+
+// calmCard.addEventListener("click", () => {
+//     window.open("./plalist-section/calm-playlist.html", "_blank");
+// });
+
+const cards = {
+    'first': './plalist-section/calm-playlist.html',
+    'second-card': './plalist-section/joyful-playlist.html',
+    'third-card': './plalist-section/romantic-playlist.html',
+    'four-card': './plalist-section/energy-playlist.html',
+    'fifth-card': './plalist-section/melancholy-playlist.html',
+    'six-card': './plalist-section/focus-playlist.html'
+};
+
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+        const link = cards[card.id];
+        if (link) window.open(link, '_blank');
+    });
+});
